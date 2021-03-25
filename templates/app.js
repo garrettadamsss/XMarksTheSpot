38d86953df5f760db306d0bcaa412343a9ac07b8
Keagan Nguyen <nguyenkeagan7@gmail.com>
2021-03-25 01:32:45 -0700

    added submit modal to location.html

--- templates/app.js
+++ templates/app.js
@@ -1,3 +1,4 @@
+
 function initMap(){
 
     // Map Center
@@ -42,7 +43,7 @@ function initMap(){
     })
   }
 
-  // erm not sure what this does
+  // This changes the text in the search bar to the selection from autocomplete
   function onPlaceChanged(){
       var place = autocomplete.getPlace();
 
@@ -54,3 +55,27 @@ function initMap(){
       }
   }
   
+//get modal element
+var modal = document.getElementById('locationModal');
+var modalBtn = document.getElementById('modalBtn');
+var closeBtn = document.getElementsByClassName('closeBtn')[0];
+
+modalBtn.addEventListener('click',openModal);
+closeBtn.addEventListener('click',closeModal);
+window.addEventListener('click',clickOutside);
+//open modal
+function openModal(){
+    modal.style.display = 'block';
+}
+
+//close modal
+
+function closeModal(){
+    modal.style.display = 'none';
+}
+
+function clickOutside(e){
+    if(e.target == modal){
+        modal.style.display = 'none';
+    }
+}
