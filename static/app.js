5541d1aaea462a05273be1461c4f7cdfb67dffc9
Long Ho <lhho@princeton.edu>
2021-03-28 03:41:27 -0400

    done

--- static/app.js
+++ static/app.js
@@ -1,51 +1,9 @@
-let marker;
-
-function initMap(){
-
-    // Map Center
-    var options = {
-      zoom:12,
-      center:{lat:33.7743,lng:-117.9380}
-    }
-
-    var map = new google.maps.Map(document.getElementById('map'),options);
-
-    // Search Autocomplete
-
-    const autocomplete = new google.maps.places.Autocomplete(document.getElementById('search_bar'));
-    autocomplete.addListener('place_changed', onPlaceChanged);
 
 
-    // idk if we need this -> optional argument in autocomplete object 
-
-    // const aOptions = {
-    //     bounds: defaultBounds,
-    //     componentRestrictions: { country: "us" },
-    //     fields: ["place_id", "geometry", "name"],
-    //     types: ["geocode"],
-    //   };
-
-    // add Marker = "{lat:___,lng:___}"
-    function addMarker(location, ){
-
-        marker = new google.maps.Marker({
-            position: location,
-            map: map
-        });
-        
-
-    }
 
 
-    // Info Window
-    const detail = new google.maps.InfoWindow({
-        content: '<h2>Yooooohoo</h2>'
-    });
 
-    marker.addListener("mouseover", () =>{
-        detail.open(map, marker);
-    })
-  }
+  
 
   // This changes the text in the search bar to the selection from autocomplete
   function onPlaceChanged(){
@@ -91,3 +49,4 @@ function clickOutside(e){
         modal.style.display = 'none';
     }
 }
+
